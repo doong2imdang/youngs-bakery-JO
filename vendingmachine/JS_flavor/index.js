d2b5fbfeca91ae14ab8a324912c48ab66303dbf0
KIm Seo Young <jmh7367@naver.com>
2024-01-10 15:37:18 +0900

    Feat: input 입력값 천단위 (,) 기능

--- vendingmachine/JS_flavor/index.js
+++ vendingmachine/JS_flavor/index.js
@@ -6,12 +6,13 @@ const walletDisplay = document.querySelector(".wallet");
 let balance = 25000;
 
 depositButton.addEventListener("click", function () {
-  const depositAmount = parseInt(amountInput.value);
+  const depositAmount = parseInt(amountInput.value.replaceAll(",", ""));
   if (!isNaN(depositAmount) && depositAmount > 0) {
     balance -= depositAmount;
     balanceDisplay.innerText =
-      (parseInt(balanceDisplay.innerText) + depositAmount).toLocaleString() +
-      "원";
+      (
+        parseInt(balanceDisplay.innerText.replaceAll(",", "")) + depositAmount
+      ).toLocaleString() + "원";
     walletDisplay.innerText = balance.toLocaleString() + "원";
 
     amountInput.value = "";
@@ -19,4 +20,11 @@ depositButton.addEventListener("click", function () {
     alert("유효한 입금 금액을 입력하세요!");
     amountInput.value = "";
   }
+});
+
+amountInput.addEventListener("keyup", function (e) {
+  let value = e.target.value;
+  value = Number(value.replaceAll(",", ""));
+  const formatValue = value.toLocaleString();
+  amountInput.value = formatValue;
 });
