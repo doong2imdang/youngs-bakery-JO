2971b2f6cd11a1d2888fe7d3110570308d90e554
KIm Seo Young <jmh7367@naver.com>
2024-01-10 22:57:40 +0900

    Feat: 음료를 클릭하면 선택된 음료 공간으로 넣어지는 기능

--- vendingmachine/JS_flavor/index.js
+++ vendingmachine/JS_flavor/index.js
@@ -3,6 +3,8 @@ const depositButton = document.querySelector(".deposit-button");
 const balanceDisplay = document.querySelector(".balance");
 const walletDisplay = document.querySelector(".wallet");
 const returnButton = document.querySelector(".return-button");
+const drinkButtons = document.querySelectorAll(".drink-item button");
+const selectedDrinksContainer = document.querySelector(".selected-drinks");
 
 let balance = 25000;
 
@@ -39,4 +41,35 @@ returnButton.addEventListener("click", function () {
   balance += parseInt(balanceDisplay.innerText.replaceAll(",", ""));
   balanceDisplay.innerText = "0원";
   walletDisplay.innerText = balance.toLocaleString() + "원";
+});
+
+drinkButtons.forEach(function (drinkButton) {
+  drinkButton.addEventListener("click", function () {
+    const drinkName = drinkButton.querySelector(".drink-name").innerText;
+
+    const existingSelectedDrink = selectedDrinksContainer.querySelector(
+      `.selected-drink[data-drink="${drinkName}"]`
+    );
+
+    if (existingSelectedDrink) {
+      const stockElement = existingSelectedDrink.querySelector(
+        ".selected-stock strong"
+      );
+      let stockCount = parseInt(stockElement.innerText);
+      stockElement.innerText = stockCount + 1;
+    } else {
+      const selectedDrinkElement = document.createElement("div");
+      selectedDrinkElement.classList.add("selected-drink");
+      selectedDrinkElement.setAttribute("data-drink", drinkName);
+
+      selectedDrinkElement.innerHTML = `
+        <img src="./images/${drinkName}.svg" alt="" />
+        <span>${drinkName}</span>
+        <div class="selected-stock">
+          <strong>1</strong>
+        </div>
+      `;
+      selectedDrinksContainer.appendChild(selectedDrinkElement);
+    }
+  });
 });
