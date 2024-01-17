9d389d8431b8ab47b6c254e28dad2460982ffcc8
KIm Seo Young <jmh7367@naver.com>
2024-01-17 18:01:34 +0900

    Feat: input value 최대, 최소값 설정

--- timer/JS_flavor/index.js
+++ timer/JS_flavor/index.js
@@ -3,6 +3,9 @@ const startButton = document.querySelector(".start-button");
 const resetButtonImage = document.querySelector(".reset-button-image");
 const resetButton = document.querySelector(".reset-button");
 const inputs = document.querySelectorAll("input");
+const hourInput = document.querySelector("#hour");
+const minuteInput = document.querySelector("#minute");
+const secondInput = document.querySelector("#second");
 
 inputs.forEach((input) => {
   input.addEventListener("focus", function () {
@@ -13,4 +16,29 @@ inputs.forEach((input) => {
     resetButton.style.color = "#ffffff";
     resetButton.style.background = "#F84A34";
   });
+
+  input.addEventListener("change", function (e) {
+    if (e.target.value < 0) {
+      alert("양수를 입력해주세요!");
+      e.target.value = "";
+    }
+  });
+});
+
+hourInput.addEventListener("change", function (e) {
+  if (e.target.value >= 100) {
+    e.target.value = 99;
+  }
+});
+
+minuteInput.addEventListener("change", function (e) {
+  if (e.target.value >= 60) {
+    e.target.value = 59;
+  }
+});
+
+secondInput.addEventListener("change", function (e) {
+  if (e.target.value >= 60) {
+    e.target.value = 59;
+  }
 });
