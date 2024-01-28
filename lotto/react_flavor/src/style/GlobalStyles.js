e3cbf4957968f9c29633d7be60b5c438fc357a08
KIm Seo Young <jmh7367@naver.com>
2024-01-28 21:03:40 +0900

    Style: GlobalStyle 설정

---
+++ lotto/react_flavor/src/style/GlobalStyles.js
@@ -0,0 +1,23 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const GlobalStyles = createGlobalStyle`
+  ${reset}
+  button {
+    border: 0;
+    background: transparent;
+    cursor: pointer;
+    padding: 0;
+    margin: 0;
+  }
+
+  :root {
+    --color-white: #ffffff;
+    --color-yellow: #FFFF00;
+    --color-red: #FF0000;
+    --color-blue: #004BFF;
+    --color-black: #000000;
+  }
+`;
+
+export default GlobalStyles;
