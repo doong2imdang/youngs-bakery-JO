ed630fbd7772d6a62a7002b02da0d2900450c3a5
KIm Seo Young <jmh7367@naver.com>
2024-02-07 15:58:55 +0900

    Feat: 마우스로 선 긋기 기능

---
+++ memo/JS_flavor/index.js
@@ -0,0 +1,32 @@
+const lineWidth = document.getElementById("line-width");
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+canvas.width = 700;
+canvas.height = 700;
+
+let isPainting = false;
+
+ctx.lineWidth = lineWidth.value;
+
+function onMove(e) {
+  if (isPainting) {
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+    return;
+  }
+  ctx.beginPath();
+  ctx.moveTo(e.offsetX, e.offsetY);
+}
+
+function onMouseDown() {
+  isPainting = true;
+}
+
+function onMouseUp() {
+  isPainting = false;
+}
+
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousedown", onMouseDown);
+canvas.addEventListener("mouseup", onMouseUp);
+canvas.addEventListener("mouseleave", onMouseUp);
