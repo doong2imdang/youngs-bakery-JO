bfe6f80a39a4b1854f9eaa555cdb158d366f269c
KIm Seo Young <jmh7367@naver.com>
2024-02-07 16:40:36 +0900

    Feat: 색상 선택 기능

--- memo/JS_flavor/index.js
+++ memo/JS_flavor/index.js
@@ -1,3 +1,7 @@
+const colorOptions = Array.from(
+  document.getElementsByClassName("color-option")
+);
+const color = document.getElementById("color");
 const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
@@ -18,11 +22,11 @@ function onMove(e) {
   ctx.moveTo(e.offsetX, e.offsetY);
 }
 
-function onMouseDown() {
+function startPainting() {
   isPainting = true;
 }
 
-function onMouseUp() {
+function cancelPainting() {
   isPainting = false;
 }
 
@@ -30,9 +34,22 @@ function onLineWidth(e) {
   ctx.lineWidth = e.target.value;
 }
 
+function onColorChange(e) {
+  ctx.strokeStyle = e.target.value;
+  ctx.fillStyle = e.target.value;
+}
+
+function onColorClick(e) {
+  color.value = e.target.dataset.color;
+  ctx.strokeStyle = e.target.dataset.color;
+  ctx.fillStyle = e.target.dataset.color;
+}
+
 canvas.addEventListener("mousemove", onMove);
-canvas.addEventListener("mousedown", onMouseDown);
-canvas.addEventListener("mouseup", onMouseUp);
-canvas.addEventListener("mouseleave", onMouseUp);
+canvas.addEventListener("mousedown", startPainting);
+canvas.addEventListener("mouseup", cancelPainting);
+canvas.addEventListener("mouseleave", cancelPainting);
 
 lineWidth.addEventListener("change", onLineWidth);
+color.addEventListener("change", onColorChange);
+colorOptions.forEach((color) => color.addEventListener("click", onColorClick));
