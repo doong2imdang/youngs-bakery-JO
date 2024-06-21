44964396520d5d91d3e3b9b5c31ddbdfaa72bbe7
KIm Seo Young <jmh7367@naver.com>
2024-06-21 14:18:00 +0900

    Feat: 상품상세페이지 이미지/스토어/상품이름/가격

--- youngsmarket/javascript/productDetail.js
+++ youngsmarket/javascript/productDetail.js
@@ -1,8 +1,15 @@
 let userTxt = document.querySelector(".user-txt");
 let btnUser = document.querySelector(".btn-user");
+let productImage = document.querySelector(".product-image img");
+let storeName = document.querySelector(".store-name");
+let productName = document.querySelector(".product-name");
+let productPrice = document.querySelector(".product-price");
+let totalPrice = document.querySelector(".total-quantity strong");
 
 // 전역변수
 let token = localStorage.getItem("token");
+let product = localStorage.getItem("product");
+console.log(product);
 
 // 유저 버튼 텍스트 업데이트 함수
 function updateUserButton() {
@@ -18,3 +25,17 @@ function updateUserButton() {
   }
 }
 updateUserButton();
+
+// 상품상세(이미지/스토어/상품이름/가격)
+function displayProductDetail() {
+  if (product) {
+    let productObj = JSON.parse(product);
+    productImage.src = productObj.img;
+    storeName.innerText = productObj.storeName;
+    productName.innerText = productObj.productName;
+    productPrice.innerText = productObj.price.toLocaleString();
+  } else {
+    console.error("No product found in localStorage.");
+  }
+}
+displayProductDetail();
