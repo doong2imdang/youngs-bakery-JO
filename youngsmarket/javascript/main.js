130f8e53f4d1df3d3ab5e2563a069afc2e31712e
KIm Seo Young <jmh7367@naver.com>
2024-06-25 23:21:00 +0900

    Feat: 이미지 순서에 맞는 dot active

--- youngsmarket/javascript/main.js
+++ youngsmarket/javascript/main.js
@@ -2,17 +2,15 @@ let btnLeftSwiper = document.querySelector(".btn-left-swiper");
 let btnRightSwiper = document.querySelector(".btn-right-swiper");
 let imagesBox = document.querySelector(".images-box");
 let images = document.querySelectorAll(".image-slider img");
+let dots = document.querySelectorAll(".dot");
 
 // 전역변수
 let outerWidth = document.body.offsetWidth;
 let totalWidth = 0;
 let click = 0;
 
-console.log(outerWidth);
-
 window.addEventListener("resize", function () {
   outerWidth = document.body.offsetWidth;
-  console.log(outerWidth);
 });
 
 // 이미지슬라이더 다음버튼
@@ -20,8 +18,7 @@ function moveToNextImage() {
   click = (click + 1) % images.length;
   totalWidth = click * outerWidth;
   imagesBox.style.transform = `translateX(-${totalWidth}px)`;
-
-  console.log("next", click);
+  updateDots();
 }
 
 // 이미지슬라이더 이전버튼
@@ -29,11 +26,11 @@ function moveToPreviousImage() {
   if (click > 0) {
     click--;
   } else {
-    click = 4;
+    click = images.length - 1;
   }
   totalWidth = click * outerWidth;
   imagesBox.style.transform = `translateX(-${totalWidth}px)`;
-  console.log("previous", click);
+  updateDots();
 }
 
 //이미지슬라이더 자동 넘기기
@@ -47,3 +44,14 @@ imagesBox.addEventListener("mouseenter", () => {
 imagesBox.addEventListener("mouseleave", () => {
   autoSlide = setInterval(moveToNextImage, 3000);
 });
+
+// 이미지에 맞는 dot active
+function updateDots() {
+  dots.forEach((dot, index) => {
+    dot.classList.remove("dot-active");
+    if (index === click) {
+      console.log(index, click);
+      dot.classList.add("dot-active");
+    }
+  });
+}
