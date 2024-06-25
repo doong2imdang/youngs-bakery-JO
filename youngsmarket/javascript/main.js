d02113ef6962208afbb57daee1274d69dd7a523c
KIm Seo Young <jmh7367@naver.com>
2024-06-25 18:36:20 +0900

    Feat: 이미지 슬라이더 자동 넘기기

--- youngsmarket/javascript/main.js
+++ youngsmarket/javascript/main.js
@@ -8,8 +8,11 @@ let outerWidth = document.body.offsetWidth;
 let totalWidth = 0;
 let click = 0;
 
+console.log(outerWidth);
+
 window.addEventListener("resize", function () {
   outerWidth = document.body.offsetWidth;
+  console.log(outerWidth);
 });
 
 // 이미지슬라이더 다음버튼
@@ -17,6 +20,8 @@ function moveToNextImage() {
   click = (click + 1) % images.length;
   totalWidth = click * outerWidth;
   imagesBox.style.transform = `translateX(-${totalWidth}px)`;
+
+  console.log("next", click);
 }
 
 // 이미지슬라이더 이전버튼
@@ -28,4 +33,8 @@ function moveToPreviousImage() {
   }
   totalWidth = click * outerWidth;
   imagesBox.style.transform = `translateX(-${totalWidth}px)`;
+  console.log("previous", click);
 }
+
+//이미지슬라이더 자동 넘기기
+let autoSlide = setInterval(moveToNextImage, 3000);
