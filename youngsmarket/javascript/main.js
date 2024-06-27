d05df73d0756dfa0bc111a961fa30dab1ef0d3bf
KIm Seo Young <jmh7367@naver.com>
2024-06-27 15:26:06 +0900

    Feat: 메인페이지 상품 전체 불러오기

--- youngsmarket/javascript/main.js
+++ youngsmarket/javascript/main.js
@@ -4,19 +4,46 @@ let imagesBox = document.querySelector(".images-box");
 let images = document.querySelectorAll(".images-box img");
 let dots = document.querySelectorAll(".dot");
 
+let productsContainer = document.querySelector(".products-container");
+
 // 전역변수
 let outerWidth = imagesBox.offsetWidth;
 let totalWidth = 0;
 let click = 0;
 let URL = "https://openmarket.weniv.co.kr/";
+let products = [];
 
 // 상품 전체 불러오기 GET 요청
 function getProducts() {
   fetch(URL + "products/")
     .then((response) => response.json())
-    .then((data) => console.log(data))
+    .then((data) => {
+      products = data.results;
+      console.log(products);
+      displayProducts();
+    })
     .catch((error) => console.error("Error fetching products:", error));
 }
+
+// product 데이터 화면에 표시
+function displayProducts() {
+  products.forEach((product) => {
+    const listItem = document.createElement("li");
+
+    const formattedPrice = Number(product.price).toLocaleString();
+    listItem.className = "product";
+    listItem.innerHTML = `
+    <button class="product-image" type="button">
+      <img src="${product.image}" alt="" />
+    </button>
+    <p class="store-name">${product.store_name}</p>
+    <strong class="product-name">${product.product_name}</strong>
+    <p class="product-price">${formattedPrice}<span>원</span></p>
+    `;
+    productsContainer.appendChild(listItem);
+  });
+}
+
 getProducts();
 
 // 너비 구하기
