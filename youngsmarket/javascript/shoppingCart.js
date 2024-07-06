94c94defb370c1531fa01448c014d3885c94d465
KIm Seo Young <jmh7367@naver.com>
2024-07-06 17:43:24 +0900

    Feat: 장바구니페이지 장바구니버튼 색상 변경

---
+++ youngsmarket/javascript/shoppingCart.js
@@ -0,0 +1,6 @@
+let iconShoppingCart = document.querySelector(".btn-shopping-cart img");
+let txtShoppingCart = document.querySelector(".btn-shopping-cart p");
+
+// 장바구니 페이지일 경우
+iconShoppingCart.src = "../images/icon-shopping-cart-2.svg";
+txtShoppingCart.style.color = "var(--color-orange)";
