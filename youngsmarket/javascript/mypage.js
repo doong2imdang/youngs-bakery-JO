88f7551bbf25a11355ed032d99133c56f17b5288
KIm Seo Young <jmh7367@naver.com>
2024-07-25 14:52:02 +0900

    Feat: 마이페이지일 경우 버튼 색 변경

---
+++ youngsmarket/javascript/mypage.js
@@ -0,0 +1,6 @@
+let iconMyPage = document.querySelector(".btn-user img");
+let txtMyPage = document.querySelector(".btn-user p");
+
+// 마이 페이지일 경우
+iconMyPage.src = "../images/icon-user-2.svg";
+txtMyPage.style.color = "var(--color-orange)";
