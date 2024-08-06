56b0d95840008efb81e43526202c1f4407cfec34
KIm Seo Young <jmh7367@naver.com>
2024-08-06 16:09:10 +0900

    Feat: 휴대폰 번호 옵션 클릭 이벤트

--- youngsmarket/javascript/signup.js
+++ youngsmarket/javascript/signup.js
@@ -3,6 +3,8 @@ let btnSales = document.querySelector(".btn-sales");
 let signupForm = document.querySelector(".signup-form");
 let businessNumberBox = document.querySelector(".business-number-box");
 let storeNameBox = document.querySelector(".store-name-box");
+let selectBoxUl = document.querySelector(".select-box ul");
+let btnArrow = document.querySelector(".btn-arrow");
 
 // 전역변수
 let signupType = "BUYER";
@@ -26,4 +28,23 @@ function btnSalesClick() {
   signupForm.classList.remove("member-check-purchasing");
   businessNumberBox.style.display = "block";
   storeNameBox.style.display = "block";
+}
+
+// 휴대폰 시작하는 번호
+function handleFirstPhoneNumberBtnClick() {
+  if (selectBoxUl.style.display === "block") {
+    selectBoxUl.style.display = "none";
+    btnArrow.classList.remove("btn-arrow-active");
+  } else {
+    selectBoxUl.style.display = "block";
+    btnArrow.classList.add("btn-arrow-active");
+  }
+}
+
+// 휴대폰 옵션 선택
+function handleOptionClick(event) {
+  const selectedValue = event.target.innerText;
+  document.querySelector(".selected-value").innerText = selectedValue;
+  selectBoxUl.style.display = "none";
+  btnArrow.classList.remove("btn-arrow-btn");
 }
