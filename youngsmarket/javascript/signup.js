08e8844a9f002fe174896bd7f2fb953ebac4293a
KIm Seo Young <jmh7367@naver.com>
2024-08-07 23:19:16 +0900

    Feat: input 값 조건별error message 출력

--- youngsmarket/javascript/signup.js
+++ youngsmarket/javascript/signup.js
@@ -9,7 +9,10 @@ let btnArrow = document.querySelector(".btn-arrow");
 // 전역변수
 let signupType = "BUYER";
 let URL = "https://openmarket.weniv.co.kr/";
+let isDoubleChecked = false;
 let isChecked = false;
+let isPhoneNumber = false;
+let isUsername = false;
 
 // 회원가입 회원 유형 선택 버튼
 function btnPurchasingClick() {
@@ -35,7 +38,27 @@ function btnSalesClick() {
 // 아이디 input
 function onInputId() {
   const idError = document.querySelector(".id-error");
-  idError.style.display = "none";
+  const inputIdValue = document.getElementById("input-id").value;
+
+  // 정규 표현식을 사용하여 유효성 검사
+  const idPattern = /^[a-zA-Z0-9]{1,20}$/;
+
+  if (idPattern.test(inputIdValue)) {
+    idError.style.display = "none";
+    isUsername = true;
+  } else {
+    idError.innerHTML =
+      "ID는 20자 이내의 영어 소문자, 대문자, 숫자만 가능합니다.";
+    idError.style.display = "block";
+    isUsername = false;
+  }
+
+  if (inputIdValue.length == 0) {
+    idError.style.display = "none";
+    isUsername = false;
+  }
+
+  console.log(isUsername);
 }
 
 // 아이디 중복 확인
@@ -55,13 +78,17 @@ function handleUsernameDoubleCheckClick() {
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
+
       if (data.FAIL_Message) {
         idError.innerHTML = data.FAIL_Message;
         idError.style.display = "block";
+        isDoubleChecked = false;
       } else {
         idError.innerHTML = data.Success;
         idError.style.display = "block";
+        isDoubleChecked = true;
       }
+      console.log(isDoubleChecked);
     });
 
   console.log(inputId, idError);
@@ -102,6 +129,16 @@ function onInputPw2() {
   }
 }
 
+// 이름
+function onInputName() {
+  const inputNameValue = document.getElementById("input-name").value;
+  const nameError = document.querySelector(".name-error");
+
+  if (inputNameValue.length > 0) {
+    nameError.style.display = "none";
+  }
+}
+
 // 휴대폰 시작하는 번호
 function handleFirstPhoneNumberBtnClick() {
   if (selectBoxUl.style.display === "block") {
@@ -123,8 +160,26 @@ function handleOptionClick(event) {
 
 // 휴대폰 번호 숫자만 가능
 function validateInput(event) {
+  const phoneNumberValues = document.querySelectorAll("#input-phone-number");
   const input = event.target;
   input.value = input.value.replace(/[^0-9]/g, "");
+  const phoneNumberError = document.querySelector(".phone-number-error");
+
+  const secondNum = phoneNumberValues[0].value.length;
+  const thirdNum = phoneNumberValues[1].value.length;
+  console.log(secondNum + thirdNum);
+
+  if (secondNum + thirdNum < 7 && secondNum + thirdNum > 0) {
+    phoneNumberError.innerText =
+      "핸드폰번호는 01*으로 시작해야 하는 10~11자리 숫자여야 합니다.";
+    phoneNumberError.style.display = "block";
+    isPhoneNumber = false;
+  } else {
+    phoneNumberError.style.display = "none";
+    isPhoneNumber = true;
+  }
+
+  console.log(isPhoneNumber);
 }
 
 // 체크 박스
@@ -146,7 +201,7 @@ function handleSignup(event) {
   // 입력 데이터
   const username = document.getElementById("input-id").value;
   const password = document.getElementById("input-pw").value;
-  const confrimPassword = document.getElementById("input-pw-check").value;
+  const confirmPassword = document.getElementById("input-pw-check").value;
   const name = document.getElementById("input-name").value;
   const phoneNumberFirst = document.querySelector(".selected-value").innerText;
   const phoneNumber = document.querySelectorAll("#input-phone-number");
@@ -156,24 +211,57 @@ function handleSignup(event) {
   const phone = `${phoneNumberFirst}${phoneNumber[0].value}${phoneNumber[1].value}`;
   const emailComplete = `${email[0].value}@${email[1].value}}`;
 
+  // 에러
+  const idError = document.querySelector(".id-error");
+  const pwError = document.querySelector(".pw-error");
+  const pwCheckError = document.querySelector(".pw2-error");
+  const nameError = document.querySelector(".name-error");
+  const phoneNumberError = document.querySelector(".phone-number-error");
+
   // 입력 데이터 객체 생성
   const signupData = {
     username: username,
     password: password,
-    password2: confrimPassword,
+    password2: confirmPassword,
     phone_number: phone,
     name: name,
   };
 
-  fetch(URL + "accounts/signup/", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(signupData),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-    });
+  if (isDoubleChecked && isChecked && isPhoneNumber && isUsername) {
+    fetch(URL + "accounts/signup/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(signupData),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        if (data.username) {
+          idError.innerText = data.username[0];
+          idError.style.display = "block";
+        }
+
+        if (data.password) {
+          pwError.innerText = data.password[0];
+          pwError.style.display = "block";
+        }
+
+        if (data.password2) {
+          pwCheckError.innerText = data.password2[0];
+          pwCheckError.style.display = "block";
+        }
+
+        if (data.name) {
+          nameError.innerText = data.name[0];
+          nameError.style.display = "block";
+        }
+
+        if (data.phone_number) {
+          phoneNumberError.innerText = data.phone_number[0];
+          phoneNumberError.style.display = "block";
+        }
+      });
+  }
 }
