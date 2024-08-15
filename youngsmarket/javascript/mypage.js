b5f6704692ae93dd638e859bb048362ea761ef9e
KIm Seo Young <jmh7367@naver.com>
2024-08-15 12:25:00 +0900

    Feat: 회원 username 값 표시

--- youngsmarket/javascript/mypage.js
+++ youngsmarket/javascript/mypage.js
@@ -1,6 +1,11 @@
 let iconMyPage = document.querySelector(".btn-user img");
 let txtMyPage = document.querySelector(".btn-user p");
+let username = document.querySelector(".username");
+
+// 전역변수
+let localUsername = localStorage.getItem("username");
 
 // 마이 페이지일 경우
 iconMyPage.src = "../images/icon-user-2.svg";
 txtMyPage.style.color = "var(--color-orange)";
+username.innerHTML = localUsername + "님";
