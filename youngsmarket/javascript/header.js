cca9b0e2677547599e1d6ddb5111b6586a659efb
KIm Seo Young <jmh7367@naver.com>
2024-08-18 23:20:38 +0900

    Feat: 말풍선 클릭 시 마이페이지 이동 및 로그아웃 기능

--- youngsmarket/javascript/header.js
+++ youngsmarket/javascript/header.js
@@ -1,6 +1,9 @@
 let userTxt = document.querySelector(".user-txt");
 let btnUser = document.querySelector(".btn-user");
 let mainLogo = document.querySelector(".main-logo");
+let speechBubble = document.querySelector(".speech-bubble");
+let goMyPage = document.querySelector(".go-mypage");
+let logout = document.querySelector(".logout");
 
 // 전역변수
 token = localStorage.getItem("token");
@@ -8,13 +11,23 @@ token = localStorage.getItem("token");
 // 유저 버튼 텍스트 업데이트 함수
 function updateUserButton() {
   if (!token) {
+    userTxt.innerHTML = "로그인";
     btnUser.addEventListener("click", () => {
       location.href = "http://127.0.0.1:5500/youngsmarket/pages/login.html";
     });
   } else {
     userTxt.innerHTML = "마이페이지";
     btnUser.addEventListener("click", () => {
-      location.href = "http://127.0.0.1:5500/youngsmarket/pages/mypage.html";
+      speechBubble.style.display = "block";
+
+      goMyPage.addEventListener("click", () => {
+        location.href = "http://127.0.0.1:5500/youngsmarket/pages/mypage.html";
+      });
+
+      logout.addEventListener("click", () => {
+        window.localStorage.removeItem("token");
+        location.reload();
+      });
     });
   }
 }
