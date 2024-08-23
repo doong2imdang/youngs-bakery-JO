088a8aabf8cc17e0c6cd1c3eaf95771547332e1e
KIm Seo Young <jmh7367@naver.com>
2024-08-23 23:03:30 +0900

    Feat: 판매자센터 판매상품목록 화면 표시

--- youngsmarket/javascript/sellerCenter.js
+++ youngsmarket/javascript/sellerCenter.js
@@ -1,7 +1,11 @@
+const infoContents = document.querySelector(".info-contents");
+
 // 전역변수
 let URL = "https://openmarket.weniv.co.kr/";
 token = localStorage.getItem("token");
+let products = [];
 
+// 판매상품 리스트 GET
 function getProducts() {
   fetch(URL + "seller/", {
     method: "GET",
@@ -12,9 +16,37 @@ function getProducts() {
   })
     .then((response) => response.json())
     .then((data) => {
+      products = data.results;
       console.log(data.results);
+      displayProducts();
     })
     .catch((error) => console.error("Error fetching products:", error));
 }
-
 getProducts();
+
+// 판매상품 리스트 화면 표시
+function displayProducts() {
+  products.forEach((product) => {
+    console.log(product);
+    const listItem = document.createElement("li");
+    const formattedPrice = Number(product.price).toLocaleString() + "원";
+    listItem.className = "info-content";
+
+    let productHTML = `
+      <img
+              src=${product.image}
+              alt=""
+            />
+            <div class="content-name">
+              <p>${product.product_name}</p>
+              <span>재고 : ${product.stock}개</span>
+            </div>
+            <strong>${formattedPrice}</strong>
+            <button class="btn-edit" type="button">수정</button>
+            <button class="btn-delete" type="button">삭제</button>
+    `;
+
+    listItem.innerHTML = productHTML;
+    infoContents.appendChild(listItem);
+  });
+}
